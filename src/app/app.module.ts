194688e564fca89f2076ddb7d4e591843da3b311
Aneesh Gokhale <dev.agokhale1@gmail.com>
2018-12-04 20:41:05 -0500

    Create login form group and validate login parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -32,7 +33,9 @@ import { GymnastDetailComponent } from './gymnasts/gymnast-detail/gymnast-detail
     imports: [
         BrowserModule,
         HttpClientModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { CurrentUserService } from '../users/services/current-user.service';
 
 @Component({
@@ -10,8 +11,15 @@ import { CurrentUserService } from '../users/services/current-user.service';
 export class LoginComponent implements OnInit {
 
     private returnUrl = '';
+    loginForm: FormGroup;
+    submitted = false;
 
-    constructor(private route: ActivatedRoute, private router: Router, private authService: CurrentUserService) {
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private authService: CurrentUserService,
+        private fb: FormBuilder
+    ) {
         // Redirect if already logged in
 
         console.log(this.authService.currentUserValue);
@@ -19,15 +27,30 @@ export class LoginComponent implements OnInit {
         if (this.authService.currentUserValue) {
             this.router.navigate(['/meets']);
         }
+
+        this.loginForm = this.fb.group({
+            username: ['', [Validators.required, Validators.minLength(3)]],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
     }
 
     ngOnInit() {
-        this.route.queryParams
-            .subscribe(params => this.returnUrl = params['return'] || '/meets');
+        this.route.queryParams.subscribe(params => this.returnUrl = params['return'] || '/meets');
+    }
+
+    get form() {
+        return this.loginForm.controls;
     }
 
     login() {
-        const success: boolean = this.authService.login('', '');
+        this.submitted = true;
+
+        if (!this.loginForm.valid) {
+            return;
+        }
+
+        const success: boolean = this.authService.login(this.form.username.value, this.form.password.value);
+        console.log(this.authService.currentUserValue);
 
         if (success) {
             this.success();
